fc7ee5113baf005e40ad0e4d92ac43f1a4ae3b2b
xye4 <xye4@buffalo.edu>
2019-04-24 18:09:43 -0400

    new

--- src/index.js
+++ src/index.js
@@ -1,28 +1,21 @@
-
 var log = [];
 var points = [];
 var point = 0;
 //console.log(log[0][0]);
-console.log(points);
+//console.log(points);
 
+setUp();
 
 var grid = clickableGrid(12, 12, function(el, row, col, i, isDoubleClick) {
   if (!isDoubleClick && !el.className) {
     el.className = "clicked1";
-    log.push(i);
-  }
-  if (checkLine(log)){
-    changeClassName();
-    point++;
-    alert("Points scored:" + " " + point);
-  }
+    log[row][col]=1;
+    winning(row,col);
 
-  if (isDoubleClick && !el.className) {
-    el.className = "niclicked";
-    log.push([i, 1]);
   }
   console.log(log);
 });
+
 
 function changeClassName(){
   var name = document.getElementsByClassName("clicked1");
@@ -31,15 +24,106 @@ function changeClassName(){
   }
 }
 
+
+
+function setUp(){
+  log = [];
+  for ( var i = 0; i<12 ; i++){
+    log[i] = [];
+    for(var z = 0 ; z < 12; z++){
+      log[i].push(0);
+    }
+  }
+  console.log(log);
+}
+
+
+function winning(row, col){
+  if(!checkForwinningOrNot(row, col)){
+  }
+  else {
+    changeClassName();
+    point++;
+    alert("Points scored:" + " " + point);
+  }
+}
+
+// function changeClassName(){
+//   var name = document.getElementsByClassName("tenclicked");
+//   for(var i = (name.length - 1); i >= 0; i--) {
+//     name[i].classList.remove("tenclicked");
+//   }
+// }
+
+function checkForwinningOrNot(row,col){
+  var listOfPostion = [];
+  if(get(row,col,0,1)+ get(row,col,0,-1)>3){
+    //listOfPostion.push(row, col);
+    //cleanMatchFive(listOfPostion);
+    del(row,col,0,1);
+    del(row,col,0,-1);
+    return true;
+  }else{
+    if(get(row,col,1,0)+ get(row, col, -1,0)>3){
+      //listOfPostion.push(row, col);
+      //cleanMatchFive(listOfPostion);
+      del(row,col,1,0);
+      del(row,col,-1,0);
+      return true;
+    }
+    else{
+      return false
+    }
+  }
+}
+
+
+
+//function cleanMatchFive(list){
+//list.pop();
+//}
+
+function get(row,col,row1,col1, /*listOfPostion*/){
+
+  if(notex(row,col) == notex(row+row1,col+col1)){
+    //listOfPostion.push(row+row1, col+col1);
+    return 1 + get(row+row1, col+col1, row1,col1);
+  }
+  else{
+    return 0
+  }
+}
+
+function del(row,col,row1,col1){
+  if (notex(row,col) == notex(row+row1, col+col1 )){
+    log[row][col]=0;
+    return del(row+row1, col+col1);
+  }
+  else{
+    return 0
+  }
+}
+
+function notex(row, col) {
+  //console.log(log);
+  if (log[row] == undefined || log[row][col] == undefined) {
+    return -1;
+  } else {
+    return log[row][col];
+  }
+}
+
+
+
+
 function clickableGrid(rows, cols, callback) {
   var i = 0;
   var grid = document.createElement("table");
   grid.className = "grid";
   for (var r = 0; r < rows; ++r) {
     var tr = grid.appendChild(document.createElement("tr"));
-    for (var c = 0; c < cols; ++c && i++) {
+    for (var c = 0; c < cols; ++c) {
       var cell = tr.appendChild(document.createElement("td"));
-      cell.innerHTML = " ";
       cell.addEventListener(
           "click",
           (function(el, r, c, i) {
@@ -47,24 +131,35 @@ function clickableGrid(rows, cols, callback) {
               // block to see if double click
               setTimeout(() => {
                 callback(el, r, c, i);
-            }, 400);
+              }, 400);
             };
           })(cell, r, c, i),
           false
       );
-      cell.addEventListener(
-          "dblclick",
-          (function(el, r, c, i) {
-            return function() {
-              callback(el, r, c, i, true); // flag for double click handle
-            };
-          })(cell, r, c, i),
-          false
-      );
+
     }
   }
   return grid;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 function checkLine(log){
@@ -90,6 +185,26 @@ function checkLine(log){
     }
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 checkLine(log);
 
